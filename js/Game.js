0b75203978ccbad0e347acfb61b1bc7177759045
Pooja0793 <72892373+Pooja0793@users.noreply.github.com>
2021-01-02 20:44:49 +0530

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,34 @@
+class Game{
+    constructor(){
+
+    }
+
+    getState(){
+
+        var gameStateRef = database.ref('gameState');
+        gameStateRef.on("value",function(data){
+            gameState = data.val();
+        },function(error){
+            console.log("Error: "+error.code)
+        });
+
+    }
+
+    update(state){
+        database.ref('/').update({
+            gameState : state
+        });
+
+    }
+
+    start(){
+        if(gameState===0){
+         player = new Player();
+         player.getCount();
+
+         form = new Form();
+         form.display();
+         
+        }
+    }
+}
